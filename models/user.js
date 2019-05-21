e6030d629aacd4100f41cdf5302187367a1a614b
Dan Trevino <dantrevino@gmail.com>
2019-05-21 07:31:18 -0700

    up bcrypt cost factor

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require("bcrypt-nodejs");
 
 // Define model
-const userSchema = new Schema ({
+const userSchema = new Schema({
   email: {
     type: String,
     unique: true,
@@ -16,17 +16,21 @@ const userSchema = new Schema ({
 
 // On Save Hook, encrypt password
 // Before saving this model, run this function
-userSchema.pre('save', function(next) {
+userSchema.pre("save", function(next) {
   // Get access to the user model
   const user = this;
 
   // Generate a salt. Then run callback
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) { return next(err); }
+  bcrypt.genSalt(12, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
 
     // Hash (encrypt) password using the salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) { return next(err); }
+      if (err) {
+        return next(err);
+      }
 
       // Overwrite plain text password with encrypted password
       user.password = hash;
@@ -37,14 +41,16 @@ userSchema.pre('save', function(next) {
 
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) { return callback(err); }
+    if (err) {
+      return callback(err);
+    }
 
     callback(null, isMatch);
   });
-}
+};
 
 // Create model class
-const ModelClass = mongoose.model('user', userSchema);
+const ModelClass = mongoose.model("user", userSchema);
 
 // Export model
 module.exports = ModelClass;
