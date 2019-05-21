914eb091346a6f4972c697623890ff50f32009bb
Dan Trevino <dantrevino@gmail.com>
2019-05-21 07:14:13 -0700

    add meta, setup for static routes

---
+++ config/index.js
@@ -0,0 +1,4 @@
+// Hold application secrets and config
+module.exports = {
+  secret: "asdfaskdfahdfhajfdhhjfjhkhj"
+};
--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
+const express = require("express");
 const http = require("http");
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const router = require('./router');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const router = require("./router");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const app = express();
 
 // DB Setup
-mongoose.connect("mongodb://localhost:27017/auth", { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected.....'))
+mongoose
+  .connect("mongodb://localhost:27017/auth", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected....."))
   .catch(err => console.log(err));
 
 // App Setup
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
 // Server Setup
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
-server.listen(PORT, function(){
-  console.log("Server running on localhost:5000......")
+server.listen(PORT, function() {
+  console.log("Server running on localhost:5000......");
 });
